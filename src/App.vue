15bb88d1e59d6d33e0e9a1d4471c50a952c698f8
Alireza Nezami <133537662+alirezanezami1@users.noreply.github.com>
2024-10-03 11:57:22 +0330

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,11 @@ import { computed, provide, ref } from 'vue';
 import Basket from './components/Basket.vue';
 import { watch } from 'vue';
 import Header from '@/components/Header.vue';
-
-
-
-
+  
 const cart = ref([])
-
-
 
 const totalPrice = computed(() => cart.value.reduce((acc, item) => acc + item.price ,0))
 const vatPrice = computed(() => Math.round((totalPrice.value * 5) / 100))
-
-
-
-
-
 
 const addToCart = (item) => {
   cart.value.push(item)
@@ -28,16 +18,10 @@ const removeFromCart = (item) => {
   cart.value.splice(cart.value.indexOf(item) , 1)
     item.isAdded = false
 }
-
-
-
-
-
+  
 watch(cart , () => {
   localStorage.setItem('cart' , JSON.stringify(cart.value))
 } , {deep: true})
-
-
 
 const basketOpen = ref(false)
 
@@ -49,7 +33,6 @@ const openBasket = () => {
   basketOpen.value = true
 }
 
-
 provide('cart' , {
   cart,
   openBasket,
@@ -58,28 +41,16 @@ provide('cart' , {
   addToCart
 })
 
-
 </script>
 
 <template>
   <Basket v-if="basketOpen" :totalPrice="totalPrice" :vat="vatPrice" ></Basket>
  
  <div class="bg-white w-full max-sm:w-content rounded-xl shadow-xl">
-    <Header :totalPrice="totalPrice"></Header>
-
-    <!-- <div class="flex w-screen  justify-center my-10 gap-2 py-5 h-max lg:h-auto  px-5 lg:py-10 lg:px-20">
-        <img src="../../public/Group 112.png" class="w-screen rounded-2xl shadow-xl cursor-pointer">
-  </div> -->
-    
+    <Header :totalPrice="totalPrice"></Header>  
     <div class="p-10">
-
      <RouterView colorSpinner="green"/>
-      
     </div>
-
   </div>
+  
 </template>
-
-<style >
-
-</style>
