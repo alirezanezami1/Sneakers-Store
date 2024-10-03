45070c1e0352570a510e9fd21e92b8e7d92558ef
Alireza Nezami <133537662+alirezanezami1@users.noreply.github.com>
2024-10-03 12:06:25 +0330

    Update CardList.vue

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -8,18 +8,10 @@ defineProps({
 })
 
 const limit = ref(4)
-
-
-
 const emit = defineEmits(['addToFavorite' , 'addToCart'])
-
-
-
 </script>
 
 <template>
-
-   
     <div class="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5" v-auto-animate>
      <Card v-for="item in items.slice(0 , limit || items.length)" :key="item.id" :id="item.id"  :imageUrl="item.imageUrl" :title="item.title" :price="item.price" :isAdded="item.isAdded" :isFavorite="item.isFavorite" :favoriteFuncs="isFavorite ? null : () => emit('addToFavorite' , item)" :addedFuncs=" isFavorite ? null :() => emit('addToCart' , item)"></Card> 
     </div>
@@ -27,11 +19,5 @@ const emit = defineEmits(['addToFavorite' , 'addToCart'])
     <div class="flex items-center justify-center mb-10 mt-16 buttonsShow" v-if="items.length">
         <button @click="limit=0" v-if="limit" class= " text-white font-bold py-2 px-4 rounded shadow-xl"> show more</button>
         <button @click="limit=4" v-if="!limit" class=" text-white font-bold py-2 px-4 rounded"> show less</button>
-    </div>
-    
-
-    
-
-
-    
+    </div>    
 </template>
