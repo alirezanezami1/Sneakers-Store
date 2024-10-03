6dcc9df8cab8e18450cefa5ee8b9fab498709add
Alireza Nezami <133537662+alirezanezami1@users.noreply.github.com>
2024-10-03 12:00:34 +0330

    Update FavoritesView.vue

--- src/views/FavoritesView.vue
+++ src/views/FavoritesView.vue
@@ -10,17 +10,13 @@ onMounted(async () => {
         const {data} = await axios.get('https://489df7edeb427ff0.mokky.dev/favorites?_relations=items')
         favorites.value = data.map((obj) => obj.item)
     } catch (error) {
-        console.log(error);
-        
+        console.log(error);  
     }
 })
 
 </script>
 
 <template>
-
     <h1 class="text-3xl font-bold mb-8" >Favorites</h1>
-
-
      <cardList :items="favorites" isFavorite ></cardList>
 </template>
